0c18ce6e32476dc7240a48e696515e9f71b3193d
PrathikshaAchary <prathiksha.23cs102@sode-edu.in>
2025-03-25 12:51:09 +0530

    Add files via upload

---
+++ Program32.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SMV
+ */
+public class Program32 {
+    public static void main(String[] args){
+        int x=0;
+        do{
+            System.out.println("Pune");
+            x++;
+        }
+        while(x<5);
+        
+        }}
+    }
+}
