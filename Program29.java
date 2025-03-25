df349d0c9aae978d485ae5130ab0f8b3bb99cc2b
PrathikshaAchary <prathiksha.23cs102@sode-edu.in>
2025-03-25 12:23:18 +0530

    Add files via upload

--- Program29.java
+++ Program29.java
@@ -11,10 +11,14 @@
 public class Program29 {
    
     public static void main(String[] args){
+  
+        char ch='a';
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
-            System.out.print("*"+" ");
+            System.out.print(ch+" ");
+            ch++;
     }
+            ch='a';
             System.out.println();
     }
 }
