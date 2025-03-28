f0a03ea6283e3a399c6e111653eb866e8b45c6ea
PrathikshaAchary <prathiksha.23cs102@sode-edu.in>
2025-03-28 10:40:51 +0530

    Add files via upload

---
+++ Pgm6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pgm1;
+
+
+ import java.util.Scanner;
+public class Pgm6 {
+   public static void main(String[] args)
+   {
+       Scanner scn= new Scanner(System.in);
+       int a=scn.nextInt();
+       int b=scn.nextInt();
+       double c=scn.nextDouble();
+       System.out.println(a+" "+b+" "+c);
+       
+   }
+}
